a66ac69d7c6aa5dfb8e8a60e2dd302eb73fe4f46
mstrmorningstar <45027285+mstrmorningstar@users.noreply.github.com>
2019-06-01 13:28:39 +0400

    Test

---
+++ precache-manifest.1edbb3ab41583ee5fed719b96001d1ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app-test/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "e3a05b118a2f3c5ccf08",
+    "url": "/vk-app-test/static/js/runtime~main.e3a05b11.js"
+  },
+  {
+    "revision": "6d5466265390a4f484b7",
+    "url": "/vk-app-test/static/js/main.6d546626.chunk.js"
+  },
+  {
+    "revision": "d441c1b1f659136a5047",
+    "url": "/vk-app-test/static/js/2.d441c1b1.chunk.js"
+  },
+  {
+    "revision": "6d5466265390a4f484b7",
+    "url": "/vk-app-test/static/css/main.41d4c010.chunk.css"
+  },
+  {
+    "revision": "d441c1b1f659136a5047",
+    "url": "/vk-app-test/static/css/2.7fdf9a67.chunk.css"
+  },
+  {
+    "revision": "b54b0e20b5905827d2330a8434c21b7a",
+    "url": "/vk-app-test/index.html"
+  }
+];
